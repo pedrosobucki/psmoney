f6d7a9f4634b135761ae40bca81b5765f634bd6f
pedrosobucki <62084173+pedrosobucki@users.noreply.github.com>
2021-11-06 20:26:09 -0300

    configurando modal de criação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root')
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true)
+    console.log('open')
+
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false)
+    console.log('close')
+
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}>
+
+        <h2>Create new transaction</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );

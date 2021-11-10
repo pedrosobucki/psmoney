3f21af1aefa4ff16d3e6f04dc830a9fc82f4fb09
pedrosobucki <62084173+pedrosobucki@users.noreply.github.com>
2021-11-09 22:25:50 -0300

    inserido transacao na api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,12 @@
 import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import { FormEvent, useState } from "react";
+import Modal from 'react-modal';
+
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import Modal from 'react-modal';
+
+import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -20,9 +23,11 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     function handleCreateNewTransaction(event: FormEvent){
         event.preventDefault();
 
-        console.log({
+        const data = {
             title, value, category, type
-        })
+        }
+
+        api.post('/transactions', data)
     }
 
     Modal.setAppElement('#root')
